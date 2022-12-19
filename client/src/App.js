bd17bbbf7cd97ef536cff2a5153310033c32d0f4
KrakovBen <93730442+KrakovBen@users.noreply.github.com>
2022-12-19 17:16:07 +0200

    Start

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import MyCard from './components/card/MyCard';
+// import SandBox from './sandbox/SandBox';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <SandBox/> */}
+      <MyCard/>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/sandbox/SandBox.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+// import InlineStyle from './InlineStyle'
+import MuiSandBox from './mui-sandbox/MuiSandBox'
+// import CompLogic from './CompLogic'
+// import StringInterpolation from './StringInterpolation'
+
+function SandBox() {
+  return (
+    <div>
+        {/* <CompLogic/> */}
+        {/* <StringInterpolation/> */}
+        {/* <InlineStyle/> */}
+        <MuiSandBox />
+    </div>
+  )
+}
+
+export default SandBox
